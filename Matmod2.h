081a8ee5495d6ae5922f620c2150c0c1e8365e41
YunseoH <hwangys@snu.ac.kr>
2023-07-04 13:34:13 +0900

    some functions are added

--- Matmod2.h
+++ Matmod2.h
@@ -37,6 +37,22 @@ public:
         }
     }
 
+    //make column vector
+    Matmod2(vector<int> y){
+        row = y.size();
+        column = 1;
+        for(auto i : y){
+            mat.push_back({(bool)i});
+        }
+    }
+
+    //with vector<vector<bool>>
+    Matmod2(vector<vector<bool>> _mat){
+        row = _mat.size();
+        column = _mat[0].size();
+        mat = _mat;
+    }
+
     //from text file to matrix.
     //only used for initializing function. The reason why this is not obvious function. (initially transpose)
     Matmod2(vector<string> data){
@@ -72,6 +88,43 @@ public:
         return A;
     }
 
+    Matmod2 operator+(Matmod2& ref){
+         if(this->row != ref.row || this->column != ref.column){
+            cout << "error: matrix size not compatible for addition" << endl;
+            return *this;
+        }
+
+        auto A = Matmod2(this->row, this->column);
+        for(int i = 0; i < A.row; i++){
+            for(int j = 0; j < A.column; j++){
+                A.mat[i][j] = (this->mat[i][j] != ref.mat[i][j]);
+            }
+        }
+
+        return A;       
+    }
+
+    void transpose(){
+        int tmp = row;
+        row = column;
+        column = tmp;
+
+        vector<vector<bool>> new_mat;
+
+        for(int i = 0; i < row; i++){
+            vector<bool> vec(column);
+            new_mat.push_back(vec);
+        }
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < column; j++){
+                new_mat[i][j] = mat[j][i];
+            }
+        }
+
+        mat = new_mat;
+    }
+
     void concat(Matmod2& ref){
         if(this->row != ref.row){
             cout << "error: matrix size not compatible for concatenation" << endl;
@@ -101,3 +154,27 @@ public:
     }
 
 };
+
+Matmod2 shuffle(Matmod2 A, vector<int> perm){
+    auto B = A;
+    B.transpose();
+    auto C = B;
+    for(int i = 0; i < perm.size(); i++){
+        C.mat[perm[i]] = B.mat[i];
+    }
+    C.transpose();
+
+    return C;
+}
+
+//cut row: a~b, column: x~y
+Matmod2 cut(Matmod2 A, int a, int b, int x, int y){
+    Matmod2 B(b-a+1, y-x+1);
+    for(int i = 0; i < B.row; i++){
+        for(int j = 0; j < B.column; j++){
+            B.mat[i][j] = A.mat[a + i][x + j];
+        }
+    }
+
+    return B;
+}
--- main.cpp
+++ main.cpp
@@ -51,13 +51,18 @@ bool init(){
 
 //gauss with only first p columns, and check their rank.
 bool gauss(Matmod2& A, Matmod2& b, int p){
+    if(p > n-k) {
+        cout << "error: two big integer" << endl;
+        return false;
+    }
+
     bool is_zero = true;
     for(int t = 0; t < p; t++){
         for(int i = 0; i < n - k; i++){
             if(A.mat[i][t]){
                 swap(A.mat[i], A.mat[t]);
                 swap(b.mat[i], b.mat[t]);
-                for(int j = i + 1; j < n; j++){
+                for(int j = i + 1; j < n - k; j++){
                     if(A.mat[j][t]){
                         for(int x = 0; x < n; x++){
                             A.mat[j][x] = (A.mat[j][x] != A.mat[t][x]);
@@ -80,17 +85,75 @@ bool gauss(Matmod2& A, Matmod2& b, int p){
 int main()
 {
     /* input matrix */
-    init();
+    if(!init()){
+        cout << "error: cannot initialize" << endl;
+        return 0;
+    }
  
-    H.print();
-    s.print();
+    int p = 4;   //choose between 0 <= p <= W, (should it be multiple of 4)
+    int l = 6;   //choose between 0 <= l <= n - k, should be k == l (mod 2)
 
-    /*
-    gauss(H, s, 8);
-    H.print();
-    s.print();
-    */
+    int d[31][31];
+    for (int i = 0; i < 31; i++) {
+        d[i][0] = 1;
+    }
+    for (int i = 1; i < 31; i++) {
+        for (int j = 1; j <= i; j++) {
+            d[i][j] = d[i - 1][j - 1] + d[i - 1][j];
+        }
+    } //pascal's triangle for combination
 
+    int p1 = p/2;
+    int p2 = p1/2;
+    int l1 = (int)log2(d[p-1][p1-1]);
+
+    vector<int> perm;
+    for(int i = 0; i < n; i++){
+        perm.push_back(i);
+    }
+
+    do {
+        auto H_bar = shuffle(H, perm);
+
+        if(!gauss(H_bar, s, n - k - l)){
+            continue;
+        }
+
+        auto H1 = cut(H_bar, 0, n-k-l-1, n-k-l, n-1);
+        auto H2 = cut(H_bar, n-k-l, n-1, n-k-l, n-1);
+        auto s1 = cut(s, 0, n-k-l-1, 0, 0);
+        auto s2 = cut(s, n-k-l, n-1, 0, 0);
+
+        vector<pair<Matmod2, Matmod2>> L1, L2, L3, L4, LL1, LL2, L;
+        vector<Matmod2> y1_set, y2_set;
+
+        vector<int> ones((k+l)/2 - p2);
+        for(int i = 0; i < p2; i++){
+            ones.push_back(1);
+        }
+        vector<int> zero((k+l)/2);
+
+        do{
+            vector<int> y1 = ones;
+            vector<int> y2 = zero;
+            y1.insert(y1.end(), zero.begin(), zero.end());
+            y2.insert(y2.end(), ones.begin(), ones.end());
+            y1_set.push_back(Matmod2(y1));
+            y2_set.push_back(Matmod2(y2));
+        }
+        while(next_permutation(y.begin(), y.end()));
+
+        for(auto y1 : y1_set){
+            L1.push_back(make_pair(y1, H1*y1));
+            L3.push_back(make_pair(y1, H1*y1));
+        }
+
+        for(auto y2 : y2_set){
+            L2.push_back(make_pair(y2, H2*y2));
+            L4.push_back(make_pair(y2, H2*y2 + s2));
+        }
+
+    } while(next_permutation(perm.begin(), perm.end()));
  
     return 0;
 }
